793066c148b4d4636fbc93c65106f0ec5b2d5806
unknown <maryambghm2@outlook.fr>
2025-05-12 14:39:47 +0200

    V1

---
+++ src/main/java/org/example/Exercice51.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Exercice51 {
+    public static void main(String[] args) {
+       /* 1. Créer une variable nom et une variable prenom
+        2. Afficher la phrase suivante "Bonjour {prenom} {NOM}."
+        3. Remplacer les valeurs entre chevrons par les variables créées
+                précédemment
+    */
+
+        String nom, prenom;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Saisir un prenom :");
+                scanner.nextLine();
+        System.out.println("Saisir un prenom :");
+
+        System.out.println("Bonjour "+ prenom+ " "+nom);
+    }
+}
